a8126f16f1e6513903101069338f905d00951828
JZims <JZimmsGames@gmail.com>
2021-03-15 18:24:09 -0400

    commit tree d8ba89f69836cb5573d32f4997ed2a216e72961e

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feetTraveled){
+  //var feetTraveled = feetTraveled;
+  if (feetTraveled <= 400){
+    return "This one is on me!";
+  }
+    else if (feetTraveled >= 2000 && feetTraveled < 2500){
+      return "I will gladly take your thirty bucks."
+    }
+    else if (feetTraveled > 2500 && feetTraveled > 2001){
+      return "No can do."
+    }
+     
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  if (city === "NYC" ? true : false){
+    return "Ok, sounds good."
+  }
+  else if (city !== "NYC" ? true : false){
+    return "No go."
+  }
+};
+
+function switchOnCharmFromTip(goodTip){
+
+switch(goodTip) {
+  case "generous":
+    return "Thank you so much.";
+    break;
+  case "not as generous":
+    return "Thank you." 
+    break;
+  default: return "Bye.";
+} 
+
+   
+  /*if (goodTip == "generous"){
+    return 'Thank you so much.'
+  }
+  else if (goodTip !== "generous" ? true : false){
+    return "Thank you."
+  }
+  else {
+  return "Bye."
+  }*/
+};
